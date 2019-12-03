688fcf723856254ebc286cef38078fd5f3fc49d1
mvdoyle <mdoyle@truecoders.io>
2019-12-03 16:00:20 -0600

    refactored

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -3,13 +3,12 @@ namespace ChristmasWithTypes
 {
     public class Christmas
     {
-        public Christmas()
-        {
-        }
-        public string Santa { get; set; }
-        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
-        public enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat }; //TODO Make the property, "Day", type enum
         public string[] Presents;
+        public string Santa { get; set; }
+        public int TreeHeight { get; set; } //TODO Make the Height property nullable
 
-    }
+        //TODO Make the property, "Day", type enum
+        public Day { /*Fill out the days of the week*/ };
+
+}
 }
