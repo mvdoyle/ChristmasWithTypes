8fc1f49b220451c426b2b92bcf5549e7e3f37889
Adam Nash <adam.nash@live.com>
2021-06-09 13:32:31 -0600

    Nullable field added for Christmas

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,7 +5,7 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
         public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
