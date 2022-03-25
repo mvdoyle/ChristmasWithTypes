eda70d5d2ea2631f29c7b87407d1df0b5a9a051b
Brittany McClellan <brittanyhines36@yahoo.com>
2022-03-24 20:46:31 -0500

    initial commit

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,10 +5,10 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable-DONE
 
-        //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        //TODO Make the property, "Day", type enum-DONE
+        public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
 
     }
 }
